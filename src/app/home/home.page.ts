0afb04c4a3fbc4c80c041585041e11d9c0dbaafe
Anilkumrswain140@ <Anilkumrswain140@gmail.com>
2021-01-06 18:56:10 +0530

    commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,196 @@
+import { Component, OnInit } from '@angular/core';
+import jsQR from 'jsqr';
+import {  ViewChild, ElementRef } from '@angular/core';
+import { ToastController, LoadingController, Platform } from '@ionic/angular';
+import {
+  Plugins,
+  PushNotification,
+  PushNotificationToken,
+  PushNotificationActionPerformed,
+} from '@capacitor/core';
+
+const { PushNotifications } = Plugins;
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage implements OnInit {
+  @ViewChild('video', { static: false }) video: ElementRef;
+  @ViewChild('canvas', { static: false }) canvas: ElementRef;
+  @ViewChild('fileinput', { static: false }) fileinput: ElementRef;
+  canvasElement: any;
+  videoElement: any;
+  canvasContext: any;
+  scanActive = false;
+  scanResult = null;
+  loading: HTMLIonLoadingElement = null;
+  constructor(
+    private toastCtrl: ToastController,
+    private loadingCtrl: LoadingController,
+    private plt: Platform
+  ) {
+    const isInStandaloneMode = () =>
+      'standalone' in window.navigator && window.navigator['standalone'];
+    if (this.plt.is('ios') && isInStandaloneMode()) {
+      console.log('I am a an iOS PWA!');
+      // E.g. hide the scan functionality!
+    }
+  }
+  ngOnInit() {
+    console.log('Initializing HomePage');
+
+    // Request permission to use push notifications
+    // iOS will prompt user and return if they granted permission or not
+    // Android will just grant without prompting
+    PushNotifications.requestPermission().then(result => {
+      if (result.granted) {
+        // Register with Apple / Google to receive push via APNS/FCM
+        PushNotifications.register();
+      } else {
+        // Show some error
+      }
+    });
+
+    PushNotifications.addListener(
+      'registration',
+      (token: PushNotificationToken) => {
+        alert('Push registration success, token: ' + token.value);
+      },
+    );
+
+    PushNotifications.addListener('registrationError', (error: any) => {
+      alert('Error on registration: ' + JSON.stringify(error));
+    });
+
+    PushNotifications.addListener(
+      'pushNotificationReceived',
+      (notification: PushNotification) => {
+        alert('Push received: ' + JSON.stringify(notification));
+      },
+    );
+
+    PushNotifications.addListener(
+      'pushNotificationActionPerformed',
+      (notification: PushNotificationActionPerformed) => {
+        alert('Push action performed: ' + JSON.stringify(notification));
+      },
+    );
+  }
+  ngAfterViewInit() {
+    this.canvasElement = this.canvas.nativeElement;
+    this.canvasContext = this.canvasElement.getContext('2d');
+    this.videoElement = this.video.nativeElement;
+  }
+ 
+  // Helper functions
+  async showQrToast() {
+    const toast = await this.toastCtrl.create({
+      message: `Open ${this.scanResult}?`,
+      position: 'top',
+      buttons: [
+        {
+          text: 'Open',
+          handler: () => {
+            window.open(this.scanResult, '_system', 'location=yes');
+          }
+        }
+      ]
+    });
+    toast.present();
+  }
+ 
+  reset() {
+    this.scanResult = null;
+  }
+ 
+  stopScan() {
+    this.scanActive = false;
+  }
+  async startScan() {
+    // Not working on iOS standalone mode!
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: { facingMode: 'environment' }
+    });
+   
+    this.videoElement.srcObject = stream;
+    // Required for Safari
+    this.videoElement.setAttribute('playsinline', true);
+   
+    this.loading = await this.loadingCtrl.create({});
+    await this.loading.present();
+   
+    this.videoElement.play();
+    requestAnimationFrame(this.scan.bind(this));
+  }
+   
+  async scan() {
+    if (this.videoElement.readyState === this.videoElement.HAVE_ENOUGH_DATA) {
+      if (this.loading) {
+        await this.loading.dismiss();
+        this.loading = null;
+        this.scanActive = true;
+      }
+   
+      this.canvasElement.height = this.videoElement.videoHeight;
+      this.canvasElement.width = this.videoElement.videoWidth;
+   
+      this.canvasContext.drawImage(
+        this.videoElement,
+        0,
+        0,
+        this.canvasElement.width,
+        this.canvasElement.height
+      );
+      const imageData = this.canvasContext.getImageData(
+        0,
+        0,
+        this.canvasElement.width,
+        this.canvasElement.height
+      );
+      const code = jsQR(imageData.data, imageData.width, imageData.height, {
+        inversionAttempts: 'dontInvert'
+      });
+   
+      if (code) {
+        this.scanActive = false;
+        this.scanResult = code.data;
+        this.showQrToast();
+      } else {
+        if (this.scanActive) {
+          requestAnimationFrame(this.scan.bind(this));
+        }
+      }
+    } else {
+      requestAnimationFrame(this.scan.bind(this));
+    }
+  }
+  captureImage() {
+    this.fileinput.nativeElement.click();
+  }
+   
+  handleFile(files: FileList) {
+    const file = files.item(0);
+   
+    var img = new Image();
+    img.onload = () => {
+      this.canvasContext.drawImage(img, 0, 0, this.canvasElement.width, this.canvasElement.height);
+      const imageData = this.canvasContext.getImageData(
+        0,
+        0,
+        this.canvasElement.width,
+        this.canvasElement.height
+      );
+      const code = jsQR(imageData.data, imageData.width, imageData.height, {
+        inversionAttempts: 'dontInvert'
+      });
+   
+      if (code) {
+        this.scanResult = code.data;
+        this.showQrToast();
+      }
+    };
+    img.src = URL.createObjectURL(file);
+  }
+}
